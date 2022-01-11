5d56311576ab4b949fabbf4fd77c150553c31d02
alexdeathway <alexdeathway@gmail.com>
2022-01-11 22:38:50 +0530

    event timeline update

--- archiver/views.py
+++ archiver/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from cluster.models import NoteModel,NoteEventModel
+from cluster.models import NoteModel,NoteEventModel,ClusterModel
 
 class Home(TemplateView):
     template_name="home.html"
 
     def get_context_data(self, **kwargs):
         context = super(Home,self).get_context_data(**kwargs)
-        context["latest_events"] = NoteEventModel.objects.all()[:5]
+        context["latest_events"] = NoteEventModel.objects.order_by('-id')[:5]
         return context
     
