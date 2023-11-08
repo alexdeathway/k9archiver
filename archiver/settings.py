f6b545fada41d594ee270aa192a4ee81119dfac5
alexdeathway <alexdeathway@gmail.com>
2023-11-08 22:56:50 +0530

    added tailwind package and cdn support for css

--- archiver/settings.py
+++ archiver/settings.py
@@ -149,12 +149,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-#     '/var/www/static/',
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
