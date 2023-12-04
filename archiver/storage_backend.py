2e78579cfcb509e91581ab6e670e2c6881e195cb
alexdeathway <alexdeathway@gmail.com>
2023-12-04 13:07:45 +0530

    [asset support ]added backblaze support for note asset

---
+++ archiver/storage_backend.py
@@ -0,0 +1,8 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+
+class MediaStorage(S3Boto3Storage):
+    location = 'media'
+    file_overwrite = False
+    default_acl = 'public-read'
+    querystring_auth=False
+
